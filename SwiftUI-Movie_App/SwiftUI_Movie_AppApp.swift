e4a97cca133f8566cf2f9f627aec3ff4750cab8f
mfadlika <mfadlika@outlook.com>
2023-04-30 20:03:42 +0700

    Movie App

---
+++ SwiftUI-Movie_App/SwiftUI_Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Movie_AppApp.swift
+//  SwiftUI-Movie_App
+//
+//  Created by Fadli M on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
